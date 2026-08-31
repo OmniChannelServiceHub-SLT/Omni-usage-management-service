ae7ada6da690c9f3a046d69c1d336b4902ad1c18
Tharindu Madushan <tharindum917@gmail.com>
2026-08-31 11:58:53 +0530

    updating and fixing Server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
